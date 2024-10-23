2eb8751cbcd0e63b0c270a6cd1d58ecec6f773e7
Mateusz <mateusz.stronydlafirm@gmail.com>
2024-10-23 09:56:13 +0200

    :ayout, pt 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,27 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        'hero-grid': '5fr 2fr',
+        'calc-input-grid': '1fr 52px 52px'
+      },
       colors: {
         primary: 'var(--aw-color-primary)',
         secondary: 'var(--aw-color-secondary)',
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        'rs-green': '#00984D',
+        'rs-light-green': '#F2FAF6'
+      },
+      backgroundColor: {
+        'rs-light-green': '#F2FAF6',
+        'rs-light-gray': '#F4F4F4',
+        'rs-red': '#BA2D2D',
+        'rs-green': '#00984D'
+      },
+      boxShadow: {
+        'rs-shadow': '0px 4px 0px #00984D',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
