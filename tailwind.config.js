792b0372a3322680b33d94429337d1b6df09944d
Mateusz <mateusz.stronydlafirm@gmail.com>
2024-10-24 15:00:29 +0200

    Layout, pt 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,19 @@ import typographyPlugin from '@tailwindcss/typography';
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    'bg-rs-light-gray',
+    'border-green-700',
+    'border-4',
+    'bg-rs-light-green'
+  ],
   theme: {
     extend: {
       gridTemplateColumns: {
         'hero-grid': '5fr 2fr',
-        'calc-input-grid': '1fr 52px 52px'
+        'calc-input-grid': '1fr 52px 52px',
+        'about-grid': '3fr 5fr',
+        'footer-grid': '2fr 2fr 1fr 1fr'
       },
       colors: {
         primary: 'var(--aw-color-primary)',
@@ -22,7 +30,10 @@ module.exports = {
         'rs-light-green': '#F2FAF6',
         'rs-light-gray': '#F4F4F4',
         'rs-red': '#BA2D2D',
-        'rs-green': '#00984D'
+        'rs-green': '#00984D',
+        'rs-dark-blue': '#030620',
+        'rs-dark-blue-2': '#030724',
+        'rs-dark-blue-3': 'rgb(9, 16, 65)'
       },
       boxShadow: {
         'rs-shadow': '0px 4px 0px #00984D',
